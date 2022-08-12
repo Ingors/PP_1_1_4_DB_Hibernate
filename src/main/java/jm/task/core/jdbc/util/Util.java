c891a3b21701fc577077133a408acffbb6db8f1d
Admin-PC\Admin <itmo1652@mail.ru>
2022-08-12 22:04:26 +0300

    Commit8/12/2022

--- src/main/java/jm/task/core/jdbc/util/Util.java
+++ src/main/java/jm/task/core/jdbc/util/Util.java
@@ -3,12 +3,16 @@ package jm.task.core.jdbc.util;
 import java.sql.Connection;
 import java.sql.DriverManager;
 import java.sql.SQLException;
+import org.hibernate.SessionFactory;
+import java.util.Properties;
+import org.hibernate.cfg.Configuration;
 
 public class Util {
     private static final String DRIVER = "com.mysql.cj.jdbc.Driver";
     private static final String HOST = "jdbc:mysql://localhost:3306/test?serverTimezone=Europe/Moscow";
     private static final String LOGIN = "root";
     private static final String PASSWORD = "1984";
+    private static SessionFactory sessionFactory;
 
     public static Connection getConnection() {
         Connection connection = null;
@@ -19,5 +23,18 @@ public class Util {
             e.printStackTrace();
         }
         return connection;
+    }
+    public static SessionFactory getSessionFactory() {
+        Properties property = new Properties();
+        property.setProperty("hibernate.connection.url", HOST);
+        property.setProperty("hibernate.connection.username", LOGIN);
+        property.setProperty("hibernate.connection.password", PASSWORD);
+        property.setProperty("hibernate.connection.driver_class", "com.mysql.cj.jdbc.Driver");
+        property.setProperty("dialect", "org.hibernate.dialect.MySQLDialect");
+        sessionFactory = new Configuration()
+                .addProperties(property)
+                .addAnnotatedClass(jm.task.core.jdbc.model.User.class)
+                .buildSessionFactory();
+        return sessionFactory;
     }
 }
